bb62a2ae5c9e92973908a9fe54506318fda1eff3
Hector Arteaga <15826661+hjarte17@users.noreply.github.com>
2023-02-25 15:24:01 -0500

    Javascript code to complete the message is ready

--- mixed-message.js
+++ mixed-message.js
@@ -3,18 +3,30 @@
 //Store the 3 random parts of our message
 const programmer = {
   skillLevel: ["novice", "intermiediate", "advanced", "expert"],
-  language: ["Python", "Ruby", "JavaScript", "Java", "C++", "C",],
-  cities: ["Medellin","Miami","Madrid","Mexico City","Morristown","Massapequa",]
-}
+  language: ["Python", "Ruby", "JavaScript", "Java", "C++", "C"],
+  cities: [
+    "Medellin",
+    "Miami",
+    "Madrid",
+    "Mexico City",
+    "Morristown",
+    "Massapequa",
+  ],
+};
 
 //get a random item from an array
-const randomMessage = (array) => array[Math.floor(Math.random() * array.length)];
+const randomMessage = (array) =>
+  array[Math.floor(Math.random() * array.length)];
 
 //display message function
 const displayMessage = () => {
-  let mixedMessage = `You ara a ${randomMessage(programmer.language)} programmer at the ${randomMessage(programmer.skillLevel)} level  in the ${randomMessage(programmer.cities)} area.`;
+  let mixedMessage = `You ara a ${randomMessage(
+    programmer.language
+  )} programmer at the ${randomMessage(
+    programmer.skillLevel
+  )} level  in the ${randomMessage(programmer.cities)} area.`;
 
   return mixedMessage;
-}
+};
 
 console.log(displayMessage(programmer));
